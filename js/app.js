9d81eaa081882ced19425ccd4c3730c010ea5506
NoeliaESanabria <noelia.sanabria@istea.com.ar>
2026-07-06 16:46:22 -0300

    se agrega manifest.json con íconos e inicio y carpeta creamos archivo - BOM para dejar una estructura limpia

--- js/app.js
+++ js/app.js
@@ -19,3 +19,17 @@ inputNota.addEventListener("keydown", function(evento) {
     inputNota.value = "";
   }
 });
+
+function actualizarEstadoConexion() {
+  if (navigator.onLine) {
+    console.log("Conectado a internet");
+  } else {
+    console.log("Sin conexión a internet");
+  }
+}
+
+window.addEventListener("online", actualizarEstadoConexion);
+window.addEventListener("offline", actualizarEstadoConexion);
+
+// Chequeo inicial al cargar la app
+actualizarEstadoConexion();
